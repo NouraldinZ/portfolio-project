62f0a2569d94c8e727cc012e82d59230e5e500b4
NouraldinZ <nzeina99@hotmail.com>
2021-05-01 19:49:36 -0600

    project folder structure and theme

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {ThemeProvider} from 'styled-components'
 import './index.css';
 import App from './App';
+import {theme} from './themes/theme';
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <ThemeProvider theme={theme}>
+    <App />  
+  </ThemeProvider>,
+
   document.getElementById('root')
 );
 
