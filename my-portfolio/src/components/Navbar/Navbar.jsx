1377f34aead1e59570e5b14096013d939ca864c3
NouraldinZ <nzeina99@hotmail.com>
2021-05-02 19:04:46 -0600

    navbar styling and logo

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import logo from "../../assets/icons/NZ.gif";
+import logo from "../../assets/icons/nnn.png";
 import "./NavbarStyle.css";
 
 export const MyNavbar = () => {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar
+        fixed="top"
+        className="animate-navbar nav-theme justify-content-between"
+        expand="md"
+        variant="dark"
+      >
         <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />
-          Reactfolio
+          <img alt="" src={logo} className="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
-            <Nav.Link href="#deets">Skills</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
+            <Nav.Link style={{ color: "white" }} href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link style={{ color: "white" }} href="#about">
+              About
+            </Nav.Link>
+            <Nav.Link style={{ color: "white" }} href="#skills">
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              style={{ color: "white" }}
+              eventKey={2}
+              href="#experience"
+            >
               Experience
             </Nav.Link>
-            <Nav.Link href="#deets">Projects</Nav.Link>
+            <Nav.Link style={{ color: "white" }} href="#projects">
+              Projects
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
