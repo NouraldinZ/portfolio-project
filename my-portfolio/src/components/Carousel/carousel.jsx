b0a2456961af58df204bcbb0b44384b5fcaed36c
Nour Zeina <nour.zeina@digitalhive.com>
2021-05-08 07:37:15 -0600

    carousel styling and effects

--- my-portfolio/src/components/Carousel/carousel.jsx
+++ my-portfolio/src/components/Carousel/carousel.jsx
@@ -8,25 +8,37 @@ import "./carouselStyle.css";
 const MyCarousel = () => {
   return (
     <div>
-      <Carousel>
-        <Carousel.Item interval={1000}>
-          <img className="d-block w-100" src={Slide1} alt="First slide" />
+      <Carousel controls={false} fade={true}>
+        <Carousel.Item interval={7500}>
+          <img
+            className="d-block w-100 custom-img"
+            src={Slide1}
+            alt="First slide"
+          />
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3>Quote Of the Day</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img className="d-block w-100" src={Slide2} alt="Second slide" />
+        <Carousel.Item interval={7500}>
+          <img
+            className="d-block w-100 custom-img"
+            src={Slide2}
+            alt="Second slide"
+          />
           <Carousel.Caption>
-            <h3>Second slide label</h3>
+            <h3>Quote Of the Day</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Slide3} alt="Third slide" />
+        <Carousel.Item interval={7500}>
+          <img
+            className="d-block w-100 custom-img"
+            src={Slide3}
+            alt="Third slide"
+          />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>Quote Of the Day</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
